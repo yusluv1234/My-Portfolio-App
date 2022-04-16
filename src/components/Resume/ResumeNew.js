7eef2a0e9bd95f4b6be9aa0af0be47b24ed403b3
yusluv1234 <yusluv124@gmail.com>
2022-04-17 01:32:10 +0800

    adding resume downloader

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,56 +1,56 @@
-import React, { useEffect, useState } from "react";
-import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import pdf from "../../Assets/Jolayemi_VisualCV_Resume.pdf";
-import Particle from "../Particle";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+// import React, { useEffect, useState } from "react";
+// import { Container, Row } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
+// import { AiOutlineDownload } from "react-icons/ai";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// import pdf from "../../Assets/Jolayemi_VisualCV_Resume.pdf";
+// import Particle from "../Particle";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://www.visualcv.com/app/cvs/6655978/preview";
+// const resumeLink =
+//   "https://www.visualcv.com/app/cvs/6655978/preview";
 
-function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+// function ResumeNew() {
+//   const [width, setWidth] = useState(1200);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+//   function onDocumentLoadSuccess({ numPages }) {
+//     setNumPages(numPages);
+//   }
+//   const [numPages, setNumPages] = useState(null);
+//   const [pageNumber, setPageNumber] = useState(1);
 
 
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+//   useEffect(() => {
+//     setWidth(window.innerWidth);
+//   }, []);
 
-  return (
-    <div>
-      <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
+//   return (
+//     <div>
+//       <Container fluid className="resume-section">
+//         <Particle />
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button variant="primary" href={pdf} target="_blank">
+//             <AiOutlineDownload />
+//             &nbsp;Download CV
+//           </Button>
+//         </Row>
 
-        <Row className="resume">
-          <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
-          </Document>
-        </Row>
+//         <Row className="resume">
+//           <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+//             <Page pageNumber={pageNumber} />
+//           </Document>
+//         </Row>
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-      </Container>
-    </div>
-  );
-}
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button variant="primary" href={pdf} target="_blank">
+//             <AiOutlineDownload />
+//             &nbsp;Download CV
+//           </Button>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
 
-export default ResumeNew;
+// export default ResumeNew;
